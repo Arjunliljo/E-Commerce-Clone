0637ead46e6e8ff44ff279fd83b9d49028bd765a
Arjun cv <arjun7180@gmail.com>
2024-05-15 01:07:31 +0530

    Made section 1

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,30 +1,28 @@
+/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import Section1 from "./Section-1";
 
 export default function Main() {
-  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+
+  const [data, setData] = useState("");
 
   //  fetching api data
   useEffect(() => {
     async function fetchData() {
-      setLoading(true);
       try {
         const res = await fetch("https://fakestoreapi.com/products?limit=20");
 
         if (!res.ok) {
           throw new Error("Failed to fetch data");
         }
-
         const data = await res.json();
 
-        console.log(data);
+        setData(data);
       } catch (error) {
         alert(error.message);
         setError(error.message);
         console.error("Error fetching data:", error.message);
-      } finally {
-        setLoading(false);
       }
     }
 
@@ -32,11 +30,13 @@ export default function Main() {
   }, []);
 
   if (error) return <Error err={error.message} />;
-  if (loading) return <Loader />;
-
+  if (!data) return <Loader />;
+  console.log(data);
   return (
     <main>
-      <Section1 />
+      <Section1 data={data.slice(0, 2)}>
+        <Button>Shop now</Button>
+      </Section1>
     </main>
   );
 }
@@ -44,6 +44,9 @@ export default function Main() {
 function Loader() {
   return <main className="loading">Loading...</main>;
 }
-function Error(err) {
+function Error({ err }) {
   <main className="loading">{err}</main>;
+}
+function Button({ children }) {
+  return <button className="main-btn">{children}</button>;
 }
--- src/Components/Section-1.jsx
+++ src/Components/Section-1.jsx
@@ -1,3 +1,39 @@
-export default function Section1() {
-  return <section id="section1">Section 1</section>;
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+export default function Section1({ data, children }) {
+  const style = {
+    // eslint-disable-next-line react/prop-types
+
+    backgroundPosition: "center",
+    backgroundSize: "55%",
+    backgroundRepeat: "no-repeat",
+  };
+
+  return (
+    <section id="section1">
+      <div
+        className="firstBgImg"
+        style={{
+          ...style,
+          backgroundImage: `url(${data[0].image})`,
+        }}
+      >
+        {" "}
+      </div>
+      <div
+        className="secondBgImg"
+        style={{
+          ...style,
+          backgroundImage: `url(${data[1].image})`,
+        }}
+      ></div>
+      <div className="home-contents">
+        <div className="flex">
+          <div style={{ fontSize: "50px", marginBottom: "2rem" }}>STRIP </div>
+          <div style={{ fontWeight: "900", fontSize: "70px" }}>CLUB</div>
+        </div>
+        {children}
+      </div>
+    </section>
+  );
 }
